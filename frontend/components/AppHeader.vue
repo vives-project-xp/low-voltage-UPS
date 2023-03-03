5d5ae666312531ec2293ac9f2156958ab5d5912a
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-03-03 12:37:44 +0100

    Initial commit of frontend using nuxt (vue)

---
+++ frontend/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/setup">Setup</NuxtLink></li>
+            </ul>
+        </nav>
+    </header>
+</template>
