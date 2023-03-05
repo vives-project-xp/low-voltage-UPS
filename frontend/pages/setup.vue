54c8d2c8e1fa628f77a6fb15b3dab88aa98b4482
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-03-05 17:05:52 +0100

    added axample content to setup page

--- frontend/pages/setup.vue
+++ frontend/pages/setup.vue
@@ -1,5 +1,27 @@
 <template>
     <section>
-      <h1 class="p-4">This page will be displayed at the /setup route.</h1>
+      <h1 class="p-4 text-center">Setup</h1>
+      <div class="flex flex-col p-4 gap-4">
+        <div class="border-2 border-black dark:border-white">
+          <h1>Step 1</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div class="border-2 border-black dark:border-white">
+          <h1>Step 2</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div class="border-2 border-black dark:border-white">
+          <h1>Step 3</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div class="border-2 border-black dark:border-white">
+          <h1>Step 4</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div class="border-2 border-black dark:border-white">
+          <h1>Step 5</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+      </div>
     </section>
 </template>
