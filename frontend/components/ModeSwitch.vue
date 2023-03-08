493d1c8e95ca9e70bbbf2f7c33c5f6f9df05d48d
tibo00 <61475999+tibo00@users.noreply.github.com>
2023-03-08 20:25:47 +0100

    dropdown dark mode button

---
+++ frontend/components/ModeSwitch.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <select class="primary-color" v-model="$colorMode.preference"> 
+      <option value="unknown" hidden="true">...</option> 
+      <option value="system">System</option>      
+      <option value="light">Light</option>      
+      <option value="dark">dark</option>         
+    </select>
+  </div>
+</template>
