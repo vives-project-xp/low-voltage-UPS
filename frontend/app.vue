70ab712efecdb9548bf6c2196a3afef19af0cb6e
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-03-09 14:40:19 +0100

    switched drawer to end (right side)

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,14 +1,9 @@
 <template>
-  <div class="drawer">
+  <div class="drawer drawer-end">
     <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
     <div class="drawer-content flex flex-col">
       <!-- Navbar -->
       <div class="flex-none sticky top-0 left-0 right-0 w-full navbar bg-base-300">
-        <div class="flex-none lg:hidden">
-          <label for="my-drawer-3" class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-          </label>
-        </div> 
         <div class="flex-1 px-2 mx-2">Navbar Title</div>
         <div class="flex-none hidden lg:block">
           <ul class="menu menu-horizontal">
@@ -18,6 +13,11 @@
             <li><NuxtLink to="/about">About</NuxtLink></li>
             <li><NuxtLink to="/contact">Contact</NuxtLink></li>
           </ul>
+        </div>
+        <div class="flex-none lg:hidden">
+          <label for="my-drawer-3" class="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+          </label>
         </div>
       </div>
       <!-- Page content -->
