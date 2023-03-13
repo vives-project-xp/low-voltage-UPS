0eeb7d582b2a6677ba904e6f133be0e1588945f7
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-03-13 23:06:57 +0100

    Moved back to layout file

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <!-- Navbar -->
+    <AppHeader class="flex-none sticky top-0 left-0 right-0" />
+    <!-- Page content -->
+		<div class="flex-auto">
+			<slot />
+		</div>
+    <!-- Footer -->
+    <AppFooter class="flex-none p-4" />
+  </div> 
+</template>
