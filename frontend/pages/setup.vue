17e0fbd8cb0f83f116f7da04eef69d0540ec9f35
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-03-13 22:53:00 +0100

    Added more content for testing purposes

--- frontend/pages/setup.vue
+++ frontend/pages/setup.vue
@@ -22,6 +22,46 @@
           <h1>Step 5</h1>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
         </div>
+        <div>
+          <h1>Step 6</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div>
+          <h1>Step 7</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div>
+          <h1>Step 8</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div>
+          <h1>Step 9</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div>
+          <h1>Step 10</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div>
+          <h1>Step 11</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div>
+          <h1>Step 12</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div>
+          <h1>Step 13</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div>
+          <h1>Step 14</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
+        <div>
+          <h1>Step 15</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+        </div>
       </div>
     </section>
 </template>
