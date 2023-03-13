2d262e3b9c6aa100cebb6908e3053aafecfac35e
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-03-13 22:58:22 +0100

    Created seperate Header and Footer modules

---
+++ frontend/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer footer-center bg-base-300 text-base-content">
+    <div>
+      <p>Copyright &copy; {{ new Date().getFullYear() }} - All right reserved by ?</p>
+    </div>
+  </footer>
+</template>
---
+++ frontend/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost normal-case text-xl">Low Voltage UPS</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/setup">Setup</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+      </ul>
+    </div>
+  </div>
+</template>
