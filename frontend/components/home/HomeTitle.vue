10d9a27e983c4dc0b0795818be1866e9833a0f06
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-03-13 23:05:54 +0100

    removed hero to fix header bug, bg opacity is gone

--- frontend/components/home/HomeTitle.vue
+++ frontend/components/home/HomeTitle.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="hero h-[500px]" style="background-image: url(/img/head.jpg);">
-    <div class="hero-overlay bg-opacity-60"></div>
-    <div class="hero-content text-center text-neutral-content">
-      <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Low Voltage UPS</h1>
-        <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam tempora dolores dolor nihil soluta repellat consequuntur quod? Nisi ipsam eligendi cum neque asperiores vero, sit iusto dolorum blanditiis excepturi. Magni!</p>
-        <button class="btn btn-primary">
-          <NuxtLink to="/setup">Get Started</NuxtLink>
-        </button>
+  <div class="h-[500px] bg-no-repeat bg-cover bg-center bg-opacity-50 bg-[url(/img/head.jpg)]">
+    <div class="flex justify-center items-center h-full">
+      <div class="text-center text-neutral-content">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">Low Voltage UPS</h1>
+          <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam tempora dolores dolor nihil soluta repellat consequuntur quod? Nisi ipsam eligendi cum neque asperiores vero, sit iusto dolorum blanditiis excepturi. Magni!</p>
+          <button class="btn btn-primary">
+            <NuxtLink to="/setup">Get Started</NuxtLink>
+          </button>
+        </div>
       </div>
     </div>
   </div>
