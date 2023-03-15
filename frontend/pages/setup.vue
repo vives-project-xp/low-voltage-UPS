2d8c10ae8d86f0c6e34ade422ed313637aa1b45b
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-03-15 17:53:58 +0100

    Added testing content to setup page

--- frontend/pages/setup.vue
+++ frontend/pages/setup.vue
@@ -1,67 +1,32 @@
 <template>
   <section>
-    <h1 class="p-4 text-center">Setup</h1>
-    <div class="flex flex-col p-4 gap-8">
-      <div>
-        <h1>Step 1</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+    <h1 class="text-2xl font-bold p-4 text-center">Setup</h1>
+    <div class="flex flex-col p-4 gap-8 items-center">
+      <div class="flex flex-col gap-4 w-72">
+        <h2 class="text-2xl font-bold text-center">1. Bluetooth</h2>
+        <div class="flex flex-col gap-2">
+          <button class="btn btn-primary w-full">Connect</button>
+        </div>
       </div>
-      <div>
-        <h1>Step 2</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+      <div class="flex flex-col gap-4 w-72">
+        <h2 class="text-2xl font-bold text-center">2. Wifi credentials</h2>
+        <div class="flex flex-col gap-2">
+          <input id="ssid" type="text" placeholder="Network Name" class="input input-bordered input-md w-full" />
+          <input type="password" placeholder="Password" class="input input-bordered input-md w-full" />
+          <button class="btn btn-primary w-full">Update</button>
+        </div>
       </div>
-      <div>
-        <h1>Step 3</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 4</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 5</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 6</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 7</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 8</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 9</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 10</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 11</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 12</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 13</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 14</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
-      </div>
-      <div>
-        <h1>Step 15</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab doloribus possimus incidunt molestiae corporis magni odit atque aperiam, vel saepe dignissimos quidem voluptate quaerat magnam fuga ratione, nulla architecto harum.</p>
+      <div class="flex flex-col gap-4 w-72">
+        <h2 class="text-2xl font-bold text-center">3. Mqtt broker</h2>
+        <div class="flex flex-col gap-2">
+          <input id="ip" type="text" placeholder="Ip address" class="input input-bordered input-md w-full" />
+          <button class="btn btn-primary w-full">Update</button>
+        </div>
       </div>
     </div>
   </section>
 </template>
+
+<script setup>
+
+</script>
