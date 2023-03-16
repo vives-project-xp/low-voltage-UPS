647d800fb773c4697c77083648486a14ff361bc3
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-03-16 13:36:00 +0100

    made bluetooth work (without module)

--- frontend/pages/setup.vue
+++ frontend/pages/setup.vue
@@ -3,13 +3,14 @@
     <h1 class="text-2xl font-bold p-4 text-center">Setup</h1>
     <!-- Alerts -->
     <div class="flex flex-col items-center gap-2">
-      <!-- Error alert -->
+      <!-- Error alert 
       <div v-if="error" class="alert alert-error shadow-lg w-fit max-w-full">
         <div>
           <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
           <span>{{error.name}}: {{error.message}}</span>
         </div>
       </div>
+      -->
       <!-- Warning alert -->
       <div v-if="!isSupported"  class="alert alert-warning shadow-lg w-fit">
         <div>
@@ -44,7 +45,7 @@
         <h2 class="text-2xl font-bold text-center">2. Wifi credentials</h2>
         <div class="flex flex-col gap-2">
           <input id="ssid" type="text" placeholder="Network Name" class="input input-bordered input-md w-full" />
-          <input type="password" placeholder="Password" class="input input-bordered input-md w-full" />
+          <input id="password" type="password" placeholder="Password" class="input input-bordered input-md w-full" />
           <button 
             class="btn btn-primary w-full"
             @click="setWifi()"
@@ -71,14 +72,39 @@
 </template>
 
 <script setup>
-import { useBluetooth } from '@vueuse/core'
 
-const {isSupported, isConnected, device, requestDevice, server, error} = useBluetooth({
-  filters: [{ namePrefix: "UPS" }],
-  optionalServices: ["000000ff-0000-1000-8000-00805f9b34fb","000000ee-0000-1000-8000-00805f9b34fb"],
-})
+let device
+let server
+let service
+let characteristic
+let isSupported
+let isConnected
+
+const requestDevice = async () => {
+  console.log('Button clicked')
+  // Check if bluetooth is supported
+  isSupported = await navigator.bluetooth.getAvailability()
+  if (!isSupported) return
+  // Request the device
+  device = await navigator.bluetooth.requestDevice({
+    filters: [{ namePrefix: "UPS" }],
+    optionalServices: ["000000ff-0000-1000-8000-00805f9b34fb"],
+  })
+  // Connect to the device
+  server = await device.gatt.connect()
+  // Check if we are connected to the device
+  isConnected = device.gatt.connected
+  if (!isConnected) return
+  // Get the service
+  service = await server.getPrimaryService('000000ff-0000-1000-8000-00805f9b34fb')
+  // Get the characteristic
+  characteristic = await service.getCharacteristic('0000ff01-0000-1000-8000-00805f9b34fb')
+  // Log the data
+  logData()
+}
 
 const setWifi = async () => {
+  logData()
   // Check if we are connected to the device
   if (!isConnected) return
   // Get the ssid and password from the input fields
@@ -86,25 +112,35 @@ const setWifi = async () => {
   const password = document.getElementById('password')
   // Check if the ssid and password are valid
   if (!ssid.value || !password.value) return
-  // Get the service and characteristic
-  const service = await server.getPrimaryService('000000ff-0000-1000-8000-00805f9b34fb')
-  const characteristic = await service.getCharacteristic('0000ff01-0000-1000-8000-00805f9b34fb')
   // Write the ssid and password to the characteristic
   await characteristic.writeValue(new TextEncoder().encode(`ssid:${ssid.value},password:${password.value}`))
+  logData()
 }
 
 const setMqtt = async () => {
+  logData()
   // Check if we are connected to the device
   if (!isConnected) return
   // Get the ip address from the input field
   const ip = document.getElementById('ip')
   // Check if the ip address is valid
   if (!ip.value) return
-  // Get the service and characteristic
-  const service = await server.getPrimaryService('000000ee-0000-1000-8000-00805f9b34fb')
-  const characteristic = await service.getCharacteristic('0000ee01-0000-1000-8000-00805f9b34fb')
   // Write the ip address to the characteristic
   await characteristic.writeValue(new TextEncoder().encode(`ip:${ip.value}`))
+  logData()
 }
+
+const logData = () => {
+  console.log("---------------------------------")
+  console.log('Is supported:', isSupported.value)
+  console.log('Is connected:', isConnected.value)
+  console.log('Device:', device)
+  console.log('Server:', server)
+  console.log('Service', service)
+  console.log('Characteristic', characteristic)
+  //console.log('Error:', error.value)
+  console.log("---------------------------------")
+}
+
 
 </script>
