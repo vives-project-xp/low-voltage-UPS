6bec913dc362ecd4c0d5b5de7f1a78defbab1b4e
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-03-23 16:52:41 +0100

    added content to about page

---
+++ frontend/components/about/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-base-200 flex flex-col">
+    <img :src="avatar" alt="avatar" class="m-8 mask mask-squircle">
+    <h3 class="text-2xl text-center">{{ name }}</h3>
+    <p class="p-8 text-center">{{ job }}</p>
+  </div>
+</template>
+
+<script setup>
+  const { name, avatar, bio } = defineProps({
+    name: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      required: true
+    },
+    job: {
+      type: String,
+      required: true
+    }
+  })
+</script>
--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,5 +1,39 @@
 <template>
   <section>
-    <h1 class="p-4">This page will be displayed at the /about route.</h1>
+    <PageTitle title="About" />
+    <!-- Who are we? -->
+    <div class="divider p-8 text-2xl font-bold text-center">Who are we ?</div>
+    <div class="mx-8 flex bg-base-300">
+      <img src="https://picsum.photos/id/176/500/400" alt="placeholder" class="p-8 w-1/3">
+      <div class="p-8 w-2/3 flex flex-col justify-center">
+        <p class="h-fit text-left">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Assumenda et neque inventore tempore similique, ab iusto laborum, quasi dolorem, harum illo nam exercitationem quod magni doloremque?
+          Quis in sint numquam?
+        </p>
+      </div>
+    </div>
+    <!-- Meet the team -->
+    <div class="divider p-8 text-2xl font-bold text-center">Meet the team</div>
+    <div class="mx-8 mb-8 flex flex-wrap justify-center gap-8">
+      <AboutCard 
+        v-for="person in team" 
+        :key="person.id"
+        :name="person.name"
+        :avatar="person.avatar"
+        :job="person.job"
+        class="flex-1"
+      />
+    </div>
   </section>
 </template>
+
+<script setup>
+  // team data
+  const team = ref([
+    {id: 0, name: 'Axel', avatar: 'https://picsum.photos/id/153/200', job: 'Hardware & Firmware'},
+    {id: 1, name: 'Maxime', avatar: 'https://picsum.photos/id/172/200', job: 'Firmware'},
+    {id: 2, name: 'Rob', avatar: 'https://picsum.photos/id/137/200', job: 'Web Site'},
+    {id: 3, name: 'Tibo', avatar: 'https://picsum.photos/id/155/200', job: 'Home Assistant Integration'},
+  ])
+</script>
