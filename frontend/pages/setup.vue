49071c39ea873e950800c3320965f11efd1ab6f0
tibo00 <61475999+tibo00@users.noreply.github.com>
2023-04-20 21:58:12 +0200

    small reformating setting page

--- frontend/pages/setup.vue
+++ frontend/pages/setup.vue
@@ -6,18 +6,8 @@
       <!-- Error alert -->
       <div v-if="error" class="alert alert-error shadow-lg w-fit max-w-full">
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="stroke-current flex-shrink-0 h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24" >
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
           <span>{{ error.name }}: {{ error.message }}</span>
         </div>
@@ -25,22 +15,11 @@
       <!-- Warning alert -->
       <div v-if="!isSupported" class="alert alert-warning shadow-lg w-fit">
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="stroke-current flex-shrink-0 h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24" >
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
           </svg>
           <span>
             <p>Warning: Your browser does not support Bluetooth!</p>
-            <p>The only Bluetooth supported browser is Google chrome.</p>
           </span>
         </div>
       </div>
@@ -50,12 +29,12 @@
           <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24" >
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
-          <span>Succesfully connected to {{ device.name }}</span>
+          <span>Successfully connected to {{ device.name }}</span>
         </div>
       </div>
     </div>
     <!-- Setup section -->
-    <div class="flex flex-col p-4 gap-8 items-center">
+    <div class="flex flex-col p-4 gap-8 items-center ">
       <!-- Bluetooth -->
       <div class="flex flex-col gap-4 w-1/4">
         <h2 class="text-2xl font-bold text-center">1. Bluetooth</h2>
