039a12ade31920336a3d05145505753adabd5890
tibo00 <61475999+tibo00@users.noreply.github.com>
2023-04-26 21:30:53 +0200

    changed style

--- frontend/components/PageTitle.vue
+++ frontend/components/PageTitle.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="tertiary-color p-12">
-        <h1 class="text-4xl font-bold text-center">{{ title }}</h1>
+    <div class="p-8">
+        <h1 class=" p-2 text-4xl font-bold text-center">{{ title }}</h1>
     </div>
 </template>
 
