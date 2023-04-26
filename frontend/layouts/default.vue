5abe4e41165caa45afd1d29291c634625b333e71
tibo00 <61475999+tibo00@users.noreply.github.com>
2023-04-26 21:32:20 +0200

    changed the default vue page width

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -3,8 +3,10 @@
     <!-- Navbar -->
     <AppHeader class="flex-none sticky top-0 left-0 right-0 " />
     <!-- Page content -->
-		<div class="flex-auto tertiary-color">
-			<slot />
+		<div class="flex-auto tertiary-color" style="background-image: linear-gradient(to right, #00b09b, #96c93d);">
+      <div class="mx-auto max-w-5xl">
+        <slot />
+      </div>
 		</div>
     <!-- Footer -->
     <AppFooter class="flex-none p-4" />
