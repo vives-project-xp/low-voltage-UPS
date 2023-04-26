2a367a2ddbf04846125c30d35514dc0b3cf89574
tibo00 <61475999+tibo00@users.noreply.github.com>
2023-04-26 21:30:05 +0200

    removed link to contact page

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -5,8 +5,7 @@
       <ul class="flex gap-4">
           <li class="py-4"><NuxtLink class="py-4" to="/">Home</NuxtLink></li>
           <li class="py-4"><NuxtLink class="py-4" to="/setup">Setup</NuxtLink></li>
-          <li class="py-4"><NuxtLink class="py-4" to="/about">About</NuxtLink></li>
-          <li class="py-4 pe-4"><NuxtLink class="py-4" to="/contact">Contact</NuxtLink></li>
+          <li class="py-4 pe-4"><NuxtLink class="py-4" to="/about">About</NuxtLink></li>
       </ul>
     </nav>
   </header>
