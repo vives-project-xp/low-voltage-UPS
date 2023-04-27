81c2c5dfdd07e0ce3b658166f9946cc14e22c86b
Tc_phoenix_ <tibo.coppens@outlook.com>
2023-04-27 13:30:07 +0200

    made more responsive

--- frontend/components/home/HomeTitle.vue
+++ frontend/components/home/HomeTitle.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="my-16">
     <div class="w-fit rounded-2xl tertiary-color-card">
-        <div class="flex flex-auto p-8 ">
-            <div class="w-1/3 mr-8 ">
+        <div class="flex md:flex-row flex-col p-8 ">
+            <div class="md:w-1/3 mr-8 md:mr-4 w-auto ms:mr-0">
                 <img class="" src="https://picsum.photos/id/176/500/400" alt="placeholder">
             </div>
-            <div class="w-2/3 text-justify ">
-                <h1 class="text-5xl font-bold py-5" > LevelUp! Your home</h1>
+            <div class="md:w-2/3 w-auto text-center ">
+                <h1 class=" divider text-5xl font-bold py-5" > LevelUp! Your home</h1>
                 <!-- add intro for project -->
-                <p>
+                <p class="text-justify">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Assumenda et neque inventore tempore similique, ab iusto laborum, quasi dolorem, harum illo nam exercitationem quod magni doloremque?
                     Quis in sint numquam?
