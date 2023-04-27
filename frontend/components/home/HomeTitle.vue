5766793cdd83cb1c8206f1e533f7ad254fa0ea28
Tc_phoenix_ <tibo.coppens@outlook.com>
2023-04-27 15:29:18 +0200

    adding responsiveness

--- frontend/components/home/HomeTitle.vue
+++ frontend/components/home/HomeTitle.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="my-16">
-    <div class="w-fit rounded-2xl tertiary-color-card">
+    <div class="w-fit rounded-2xl tertiary-color-card mx-16">
         <div class="flex md:flex-row flex-col p-8 ">
             <div class="md:w-1/3 mr-8 md:mr-4 w-auto ms:mr-0">
-                <img class="" src="https://picsum.photos/id/176/500/400" alt="placeholder">
+                <img src="https://picsum.photos/id/176/500/400" alt="placeholder">
             </div>
             <div class="md:w-2/3 w-auto text-center ">
-                <h1 class=" divider text-5xl font-bold py-5" > LevelUp! Your home</h1>
+                <h1 class=" divider text-2xl md:text-4xl font-bold py-5" > LevelUp! Your home</h1>
                 <!-- add intro for project -->
                 <p class="text-justify">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
--- frontend/pages/setup.vue
+++ frontend/pages/setup.vue
@@ -3,7 +3,7 @@
     <PageTitle title="Setup" />
     <div>
       <!-- Alerts -->
-      <div class="flex flex-col items-center gap-2 m-2">
+      <div class="flex flex-col items-center gap-2 m-2 mx-16">
           <!-- Error alert -->
           <div v-if="error" class="alert alert-error shadow-lg w-fit max-w-full">
             <div>
@@ -34,64 +34,66 @@
             </div>
           </div>
         </div>
-      <div class="rounded-2xl tertiary-color-card w-fit mx-auto m-8">
-        <!-- Setup form -->
-        <div class="flex flex-col p-4  items-center ">
-          <!-- Bluetooth -->
-          <h2 class="divider p-2 text-2xl font-bold text-center">Bluetooth</h2>
-          <button v-if="!isConnected" id="BtnConnect" class="btn btn-primary w-full" @click="requestDevice()" >
-            Connect
-          </button>
-          <button v-if="isConnected" id="BtnConnect" class="btn btn-primary w-full" @click="DisconnectDevice()" >
-            DisConnect
-          </button>
+      <div class="mx-16">
+        <div class="rounded-2xl tertiary-color-card  w-fit mt-8 m-16 mx-auto">
+          <!-- Setup form -->
+          <div class="flex flex-col p-4  items-center ">
+            <!-- Bluetooth -->
+            <h2 class="divider p-2 text-2xl font-bold text-center">Bluetooth</h2>
+            <button v-if="!isConnected" id="BtnConnect" class="btn btn-primary w-full" @click="requestDevice()" >
+              Connect
+            </button>
+            <button v-if="isConnected" id="BtnConnect" class="btn btn-primary w-full" @click="DisconnectDevice()" >
+              DisConnect
+            </button>
 
-          <!-- Wifi -->
-          <div class="flex flex-col gap-4 ">
-            <h2 class="divider p-2 text-2xl font-bold text-center">Wifi credentials</h2>
-            <div class="flex flex-col gap-2">
-              <div>
-                <input id="ssid" type="text" placeholder="Network SSID Name" class="input input-bordered input-md w-full my-1"/>
-                <input id="password" type="password" placeholder="Password" class="input input-bordered input-md w-11/12 mb-1"/>
-                <button class="btn btn-info w-1/12 p-2 mb-1" @click="toggleShowPass()">
-                  <i v-if="!showPass">
-                    <svg class="h-6 w-6 inline-flex " fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                      <g id="SVGRepo_iconCarrier"> 
-                        <path d="M12 5C5.63636 5 2 12 2 12C2 12 5.63636 19 12 19C18.3636 19 22 12 22 12C22 12 18.3636 5 12 5Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> 
-                        <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> 
-                      </g>
-                    </svg>
-                  </i>
-                  <i v-if="showPass">
-                    <svg class="h-6 w-6 inline-flex " fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                      <g id="SVGRepo_iconCarrier"> 
-                        <path d="M20 14.8335C21.3082 13.3317 22 12 22 12C22 12 18.3636 5 12 5C11.6588 5 11.3254 5.02013 11 5.05822C10.6578 5.09828 10.3244 5.15822 10 5.23552M12 9C12.3506 9 12.6872 9.06015 13 9.17071C13.8524 9.47199 14.528 10.1476 14.8293 11C14.9398 11.3128 15 11.6494 15 12M3 3L21 21M12 15C11.6494 15 11.3128 14.9398 11 14.8293C10.1476 14.528 9.47198 13.8524 9.1707 13C9.11386 12.8392 9.07034 12.6721 9.04147 12.5M4.14701 9C3.83877 9.34451 3.56234 9.68241 3.31864 10C2.45286 11.1282 2 12 2 12C2 12 5.63636 19 12 19C12.3412 19 12.6746 18.9799 13 18.9418" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                      </g>
-                    </svg>
-                  </i>
-                </button>
-                <button class="btn btn-primary w-full" @click="setWifi()">
-                  Send Wifi credentials
-                </button>
+            <!-- Wifi -->
+            <div class="flex flex-col gap-4 ">
+              <h2 class="divider p-2 text-2xl font-bold text-center">Wifi credentials</h2>
+              <div class="flex flex-col gap-2">
+                <div>
+                  <input id="ssid" type="text" placeholder="Network SSID Name" class="input input-bordered input-md w-full my-1"/>
+                  <input id="password" type="password" placeholder="Password" class="input input-bordered input-md w-11/12 mb-1"/>
+                  <button class="btn btn-info w-1/12 p-2 mb-1" @click="toggleShowPass()">
+                    <i v-if="!showPass">
+                      <svg class="h-6 w-6 inline-flex " fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier"> 
+                          <path d="M12 5C5.63636 5 2 12 2 12C2 12 5.63636 19 12 19C18.3636 19 22 12 22 12C22 12 18.3636 5 12 5Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> 
+                          <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> 
+                        </g>
+                      </svg>
+                    </i>
+                    <i v-if="showPass">
+                      <svg class="h-6 w-6 inline-flex " fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier"> 
+                          <path d="M20 14.8335C21.3082 13.3317 22 12 22 12C22 12 18.3636 5 12 5C11.6588 5 11.3254 5.02013 11 5.05822C10.6578 5.09828 10.3244 5.15822 10 5.23552M12 9C12.3506 9 12.6872 9.06015 13 9.17071C13.8524 9.47199 14.528 10.1476 14.8293 11C14.9398 11.3128 15 11.6494 15 12M3 3L21 21M12 15C11.6494 15 11.3128 14.9398 11 14.8293C10.1476 14.528 9.47198 13.8524 9.1707 13C9.11386 12.8392 9.07034 12.6721 9.04147 12.5M4.14701 9C3.83877 9.34451 3.56234 9.68241 3.31864 10C2.45286 11.1282 2 12 2 12C2 12 5.63636 19 12 19C12.3412 19 12.6746 18.9799 13 18.9418" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </g>
+                      </svg>
+                    </i>
+                  </button>
+                  <button class="btn btn-primary w-full" @click="setWifi()">
+                    Send Wifi credentials
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <!-- Mqtt broker -->
-          <div class="flex flex-col gap-4 w-full">
-            <h2 class="divider p-2 text-2xl font-bold text-center">Mqtt broker</h2>
-            <div class="flex flex-col gap-2">
-              <div>
-                <input disabled type="text" placeholder="mqtt://" value="mqtt://" class="input input-bordered input-md w-1/4" />
-                <input id="ip" type="text" placeholder="Ip address or hostname" class="input input-bordered input-md w-2/4" />
-                <input id="port" type="text" placeholder="1883" class="input input-bordered input-md w-1/4" />
+            <!-- Mqtt broker -->
+            <div class="flex flex-col gap-4 w-full">
+              <h2 class="divider p-2 text-2xl font-bold text-center">Mqtt broker</h2>
+              <div class="flex flex-col gap-2">
+                <div>
+                  <input disabled type="text" placeholder="mqtt://" value="mqtt://" class="input input-bordered input-md w-1/4" />
+                  <input id="ip" type="text" placeholder="Ip address or hostname" class="input input-bordered input-md w-2/4" />
+                  <input id="port" type="text" placeholder="1883" class="input input-bordered input-md w-1/4" />
+                </div>
+                <button class="btn btn-primary w-full" @click="setMqtt()">
+                  send Mqtt ip address
+                </button>
               </div>
-              <button class="btn btn-primary w-full" @click="setMqtt()">
-                send Mqtt ip address
-              </button>
             </div>
           </div>
         </div>
